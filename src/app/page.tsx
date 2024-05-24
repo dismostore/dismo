120164c735e643703489cda58c3079eecfc0b6a6
dismostore <application@dismo.co.uk>
2024-05-24 11:58:37 +0100

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import MaxWidthWrapper from "@/src/components/MaxWidthWrapper";
 import CTA from "../components/CTA";
-import NewsletterForm from "../components/Newsletter";
 
 export default function Home() {
   return (
